c206d6eaa4c92c3d26566ae606ab5afc1813cca4
namedyangfan <fan@chalk.com>
2018-05-12 18:18:14 -0400

    feat: creat product and update product name

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,16 @@ Rails.application.routes.draw do
         post '/login', action: :create
         delete '/logout', action: :destroy
       end
-
     end
+
+    namespace :admin do
+      controller :products do
+        get '/products', action: :index
+        post '/products', action: :create
+        patch '/products/:product_id/update_name', action: :update_name
+      end
+    end
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
