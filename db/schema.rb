c2eb885f99f1b69ddad90a4a9f54aa986694feaa
Fan Yang <cedric.yf@gmail.com>
2018-06-18 14:34:50 -0400

    add attribute to Size

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180617134012) do
+ActiveRecord::Schema.define(version: 20180618180014) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -80,6 +80,14 @@ ActiveRecord::Schema.define(version: 20180617134012) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sizes", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_sizes_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
